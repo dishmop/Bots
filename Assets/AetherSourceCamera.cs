85a02c9f52d44b8a989ec821b647d318fc6f67b5
Diarmid Campbell <dac70@cam.ac.uk>
2015-08-21 15:16:41 +0100

    Rendering of scene into heat map works

---
+++ Assets/AetherSourceCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AetherSourceCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Camera>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
